5c63e5a204e206cd3447ea2d6d41885fe371d710
Sai Dinesh Bellamkonda <sb20afw@herts.ac.uk>
2023-06-15 17:52:57 +0100

    Resolved Bugs

--- src/components/AboutContent.js
+++ src/components/AboutContent.js
@@ -1,11 +1,25 @@
 import './AboutContentStyles.css'
 import { Link } from 'react-router-dom'
+import react1 from '../assets/react1.jpg'
+import react2 from '../assets/react2.jpg'
 
 import React from 'react'
 
 const AboutContent = () => {
   return (
     <div className='about'>
+        <div className='right'>
+            <div className='img-container'>
+                <div className='img-stack top'>
+                    <img src={react1} className='img' alt='true'/>
+                </div>
+                <div className='img-stack bottom'>
+                    <img src={react2} className='img' alt='true'/>
+                </div>
+            </div>
+        </div>
+
+
         <div className='left'>
            <h1>My Skills</h1> <br /> <br />
            <div className='ta'>
