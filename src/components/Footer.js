f16b0c3114a4c954ee366c6213e421ac4c1a164e
Sai Dinesh Bellamkonda <sb20afw@herts.ac.uk>
2023-06-15 18:40:34 +0100

    Updated feature footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,6 @@
+import backimg from '../assets/backimg.png'
 import "./FooterStyles.css";
+import { Link } from 'react-router-dom';
 
 import React from 'react'
 import {FaHome, FaMailBulk, FaPhone} from "react-icons/fa";
@@ -9,31 +11,28 @@ const Footer = () => {
         <div className="footer-container">
             <div className="left">
                 <div className="location">
-                    <FaHome size={20} style={{color:"#fff", marginRight: "2rem" }} />
+                    <FaHome size={20} style={{color:"white", marginRight: "2rem" }} />
                     <div>
-                        <p>110A Burges Road, London</p>
-                        <p>United Kingdom</p>
+                        <p style={{color:"white"}}>110A Burges Road, London</p>
+                        <p style={{color:"white"}}>United Kingdom</p>
                     </div>
                 </div>
                 <div className="phone">
-                    <h4><FaPhone size={20} style={{color:"#fff", marginRight: "2rem" }} /> 
+                    <h4 style={{color:"white"}}><FaPhone size={20} style={{color:"white", marginRight: "2rem" }} /> 
                 +44 7747736635 </h4>
                 </div>
                 <div className="email">
-                    <h4><FaMailBulk size={20} style={{color:"#fff", marginRight: "2rem" }} /> 
+                    <h4 style={{color:"white"}}><FaMailBulk size={20} style={{color:"white", marginRight: "2rem" }} /> 
                 dineshbellamkonda@icloud.com </h4>
                 </div>
             </div>
             <div className="right">
-                <h4>About Me</h4>
-                <p>Goal-oriented and extensively proficient
-                     web developer with over 3 years of experience. 
-                     Committed to delivering exceptional web solutions 
-                     and creating seamless user experiences. 
-                </p>
+                <br/> <br/>
+                <Link to="/Contact" className="btn btn-light">Contact Me</Link>
             </div>
         </div>
     </div>
+    
   )
 }
 
