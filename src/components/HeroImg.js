14c89bbe4b96107bea5345da0340b10a6f6b1f42
Sai Dinesh Bellamkonda <136730015+dineshbellamkonda12@users.noreply.github.com>
2023-09-06 19:06:42 +0100

    Added Features

--- src/components/HeroImg.js
+++ src/components/HeroImg.js
@@ -12,12 +12,14 @@ export const HeroImg = () => {
            <img className="into-img" src={IntroImg} alt="intro" /> 
         </div>
         <div className="content">
-            <p>Sai Dinesh Bellamkonda</p>
-            <h1>Full Stack Developer</h1>
-            <div>
-            <Link to="/Project" className="btn">Projects</Link>
-            <Link to="/Contact" className="btn btn-light">Contact</Link>
-        </div>
+            <p className="homep">Sai Dinesh Bellamkonda</p>
+            <h1>Full Stack Web Developer</h1>
+            <p style={{ fontSize: '18px'}}>I'm Sai Dinesh Bellamkonda, a goal-oriented Full Stack Developer with nearly 4 years of experience. I specialize in crafting exceptional web solutions, leveraging technologies such as HTML5, CSS3, BootStrap, JavaScript, JQuery, TypeScript, React, Redux, Python, Django, and My SQL. I thrive in dynamic environments, utilizing Azure DevOps for deployments and Agile methodologies for efficient project management. Explore my portfolio to see my latest work, and feel free to connect with me for potential collaborations. Thank you for visiting!</p>
+            <br/><br/>
+          <div>
+              <Link to="/Project" className="btn">Projects</Link>
+              <Link to="/Contact" className="btn btn-light">Contact</Link>
+          </div>
         </div>
     </div>
   )
