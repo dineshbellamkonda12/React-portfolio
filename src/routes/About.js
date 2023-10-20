6fb4b4d057ee0adb01292f9c0449963a4f578515
Sai Dinesh Bellamkonda <136730015+dineshbellamkonda12@users.noreply.github.com>
2023-10-20 16:46:12 +0100

    Modified Page

--- src/routes/About.js
+++ src/routes/About.js
@@ -8,7 +8,7 @@ const About = () => {
   return (
     <div>
       <Navbar />
-      <HeroImgp heading="About Me" text="Goal-oriented and extensively proficient web developer with 3 years and 8 months of experience. 
+      <HeroImgp heading="About Me" text="Goal-oriented and extensively proficient web developer. 
       Committed to delivering exceptional web solutions and creating seamless user experiences. 
       Seeking a challenging role in a dynamic organization where I can utilize my technical expertise and creativity to contribute 
       to innovative web projects and drive business growth."/>
