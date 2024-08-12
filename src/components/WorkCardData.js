137294ef0c6d19173ff32aa1291eda2f87265048
Dinesh Bellamkonda <dines@MacBook-Pro.local>
2024-08-12 10:32:50 +0100

    Added To Do List API Project

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -1,94 +1,100 @@
-import pro2 from '../assets/project2.png';
-import pro5 from '../assets/DataFilter.png';
-import pro6 from '../assets/vertumotors.png';
-import pro7 from '../assets/reactportfolio.png';
-import pro8 from '../assets/ObjectDetection.png';
-import pro9 from '../assets/weatherapp.png';
-import pro10 from '../assets/reacticongame.png';
-import pro11 from '../assets/rainfallprediction.png';
-import pro12 from '../assets/HeartDisease.png';
-import pro13 from '../assets/spending_monitor.png';
-import pro14 from '../assets/MovieSystem.png'
-
+import pro2 from "../assets/project2.png";
+import pro5 from "../assets/DataFilter.png";
+import pro6 from "../assets/vertumotors.png";
+import pro7 from "../assets/reactportfolio.png";
+import pro8 from "../assets/ObjectDetection.png";
+import pro9 from "../assets/weatherapp.png";
+import pro10 from "../assets/reacticongame.png";
+import pro11 from "../assets/rainfallprediction.png";
+import pro12 from "../assets/HeartDisease.png";
+import pro13 from "../assets/spending_monitor.png";
+import pro14 from "../assets/MovieSystem.png";
+import pro15 from "../assets/todolistapi.png";
 
 const ProjectCardData = [
-    {
-        imgsrc: pro11,
-        title: "Rainfall Data Prediction and Visualisation",
-        text: "Visualization App is a web application that allows users to to be able to interact and filter data from the chart based on their condition, This app also allows users to predict rainfall data based on the given time range. It is built using Python Django and Linear Regression Machine Learning Model for the backend and HTML, CSS and Javascript for the frontend",
-        view: "https://github.com/dineshbellamkonda12/Data-Visualization-Tool-Web-Based-.git",
-    },
+  {
+    imgsrc: pro11,
+    title: "Rainfall Data Prediction and Visualisation",
+    text: "Visualization App is a web application that allows users to to be able to interact and filter data from the chart based on their condition, This app also allows users to predict rainfall data based on the given time range. It is built using Python Django and Linear Regression Machine Learning Model for the backend and HTML, CSS and Javascript for the frontend",
+    view: "https://github.com/dineshbellamkonda12/Data-Visualization-Tool-Web-Based-.git",
+  },
 
-    {
-        imgsrc: pro5,
-        title: "DataFilter",
-        text: "Data Filter is a web application that allows users to to be able to interact, extract and filter data from the table based on their condition. It is built using Python Django for the backend and HTML, CSS and Javascript for the frontend",
-        view: "https://github.com/dineshbellamkonda12/JLR-Coding-Challenge-Data-Viewer.git",
-    },
+  {
+    imgsrc: pro5,
+    title: "DataFilter",
+    text: "Data Filter is a web application that allows users to to be able to interact, extract and filter data from the table based on their condition. It is built using Python Django for the backend and HTML, CSS and Javascript for the frontend",
+    view: "https://github.com/dineshbellamkonda12/JLR-Coding-Challenge-Data-Viewer.git",
+  },
 
-    {
-        imgsrc: pro6,
-        title: "Cars Website (Clone of vertumotors.com)",
-        text: "This website is designed and developed using HTML 5, CSS 3 and Plain Vanilla JavaScript, Clone of vertumotors.com. This website is compatible for both mobile and desktop.",
-        view: "https://github.com/dineshbellamkonda12/Vertu-Motors-Website.git",
-    },
+  {
+    imgsrc: pro6,
+    title: "Cars Website (Clone of vertumotors.com)",
+    text: "This website is designed and developed using HTML 5, CSS 3 and Plain Vanilla JavaScript, Clone of vertumotors.com. This website is compatible for both mobile and desktop.",
+    view: "https://github.com/dineshbellamkonda12/Vertu-Motors-Website.git",
+  },
 
-    {
-        imgsrc: pro13,
-        title: "Spending Monitor",
-        text: "The application was developed utilizing HTML 5, CSS 3, React Hooks and Context API, It is used to keep track of your spending expenses",
-        view: "https://github.com/dineshbellamkonda12/Spending-Monitor.git",
-    },
+  {
+    imgsrc: pro13,
+    title: "Spending Monitor",
+    text: "The application was developed utilizing HTML 5, CSS 3, React Hooks and Context API, It is used to keep track of your spending expenses",
+    view: "https://github.com/dineshbellamkonda12/Spending-Monitor.git",
+  },
 
-    {
-        imgsrc: pro9,
-        title: "Weather App",
-        text: "This website is designed and developed using HTML 5, CSS 3, JavaScript(ES6) and React JS. Used Open Weather API to get the live weather data. This Application was deployed in Netlify using CI/CD Pipelines",
-        view: "https://github.com/dineshbellamkonda12/Weather-App.git",
-    },
+  {
+    imgsrc: pro9,
+    title: "Weather App",
+    text: "This website is designed and developed using HTML 5, CSS 3, JavaScript(ES6) and React JS. Used Open Weather API to get the live weather data. This Application was deployed in Netlify using CI/CD Pipelines",
+    view: "https://github.com/dineshbellamkonda12/Weather-App.git",
+  },
 
-    {
-        imgsrc: pro8,
-        title: "Object Detection",
-        text: "Object Detection Application is a real-time object detection system in computer vision. It's a popular deep learning application that can identify and locate multiple objects in an image or video frame with high accuracy and speed",
-        view: "https://github.com/dineshbellamkonda12/Object-Detection-App-Using-Yolo.git",
-    },
+  {
+    imgsrc: pro8,
+    title: "Object Detection",
+    text: "Object Detection Application is a real-time object detection system in computer vision. It's a popular deep learning application that can identify and locate multiple objects in an image or video frame with high accuracy and speed",
+    view: "https://github.com/dineshbellamkonda12/Object-Detection-App-Using-Yolo.git",
+  },
 
-    {
-        imgsrc: pro10,
-        title: "Game Play with React Icon",
-        text: "'Game Play with React Icon' is an app where you can interact and play with react icon. This app was developed using HTML 5, CSS 3, JavaScript and React JS",
-        view: "https://github.com/dineshbellamkonda12/React-Icon-Mini-Project.git",
-    },
+  {
+    imgsrc: pro10,
+    title: "Game Play with React Icon",
+    text: "'Game Play with React Icon' is an app where you can interact and play with react icon. This app was developed using HTML 5, CSS 3, JavaScript and React JS",
+    view: "https://github.com/dineshbellamkonda12/React-Icon-Mini-Project.git",
+  },
 
-    {
-        imgsrc: pro7,
-        title: "My Portfolio",
-        text: "This website is designed and developed using HTML 5, CSS 3, JavaScript(ES6) and React JS. This Application was deployed in Azure Devops utilizing CI/CD pipelines",
-        view: "https://github.com/dineshbellamkonda12/React-portfolio.git",
-    },
+  {
+    imgsrc: pro7,
+    title: "My Portfolio",
+    text: "This website is designed and developed using HTML 5, CSS 3, JavaScript(ES6) and React JS. This Application was deployed in Azure Devops utilizing CI/CD pipelines",
+    view: "https://github.com/dineshbellamkonda12/React-portfolio.git",
+  },
 
-    {
-        imgsrc: pro2,
-        title: "AA Cloud Solutions",
-        text: "The application was developed utilizing HTML 5, CSS 3, React and WordPress. Integrated HubSpot Plugin for chat facility and Used Formintor Plugin for Forms",
-        view: "https://aacloudsolutions.co.uk/",
-    },
+  {
+    imgsrc: pro2,
+    title: "AA Cloud Solutions",
+    text: "The application was developed utilizing HTML 5, CSS 3, React and WordPress. Integrated HubSpot Plugin for chat facility and Used Formintor Plugin for Forms",
+    view: "https://aacloudsolutions.co.uk/",
+  },
 
-    {
-        imgsrc: pro12,
-        title: "Heart Disease Prediction using Machine Learning",
-        text: "This application uses machine learning models, including K-Nearest Neighbour, Random Forest, Gradient Boosting, and Logistic Regression, to detect and predict heart disease.",
-        view: "https://github.com/dineshbellamkonda12/Heart-Disease-Detection-and-Prediction.git",
-    },
+  {
+    imgsrc: pro12,
+    title: "Heart Disease Prediction using Machine Learning",
+    text: "This application uses machine learning models, including K-Nearest Neighbour, Random Forest, Gradient Boosting, and Logistic Regression, to detect and predict heart disease.",
+    view: "https://github.com/dineshbellamkonda12/Heart-Disease-Detection-and-Prediction.git",
+  },
 
-    {
-        imgsrc: pro14,
-        title: "Movie Tracking System",
-        text: "Movie Tracking System helps users to search the movie database and filter results based on genre preferences. Technologies used are React (version 18.2.0) BootStrap (Version 5.3.2) npm (Node package manager) Git",
-        view: "https://github.com/dineshbellamkonda12/Movie-Tracking-System.git",
-    },
+  {
+    imgsrc: pro14,
+    title: "Movie Tracking System",
+    text: "Movie Tracking System helps users to search the movie database and filter results based on genre preferences. Technologies used are React (version 18.2.0) BootStrap (Version 5.3.2) npm (Node package manager) Git",
+    view: "https://github.com/dineshbellamkonda12/Movie-Tracking-System.git",
+  },
 
-]
+  {
+    imgsrc: pro15,
+    title: "To Do List API",
+    text: "This API allows you to manage tasks and projects. It supports CRUD operations for tasks and projects, task assignment to projects, and various filtering and searching functionalities.",
+    view: "https://github.com/dineshbellamkonda12/todoListAPI.git",
+  },
+];
 
 export default ProjectCardData;
